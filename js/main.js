86f8bb8c1772276b90feb0914b039879e86365c2
salvacam <salvacam@gmail.com>
2019-12-05 00:17:13 +0100

    rotate orientation

--- js/main.js
+++ js/main.js
@@ -2,20 +2,62 @@ document.addEventListener('DOMContentLoaded', function () {
   	app.init();
 });
 
+
+  
+
+
 var app = {  
   	note: document.getElementById('note'),
   	clearNotes: document.getElementById('clearNotes'),
   	modalClear: document.getElementById('modalClear'),
+
+    doOnOrientationChange: function() {
+      
+/*
+        alert("The orientation of the screen is: " + screen.orientation.type);
+        alert("The orientation of the screen is: " + screen.orientation.angle);
+        */
+        
+      switch(screen.orientation.angle) {  
+        case 0:      
+          //portrait-primary
+          screen.lockOrientation("portrait-primary");
+          break; 
+            
+        case 180:  
+          //portrait-secondary
+          screen.lockOrientation("portrait-secondary");
+          break; 
+      
+        case 90:  
+          //landscape-primary
+          screen.lockOrientation("landscape-primary");
+          break;  
+      
+        case 270:  
+          //landscape-secondary
+          screen.lockOrientation("landscape-secondary");
+          break;
+      }
+    },
   
-  	init: function() {  		
+  	init: function() {  	
+
+      app.doOnOrientationChange();
+      window.addEventListener('orientationchange', app.doOnOrientationChange);
+
   		app.note.addEventListener('keyup',
   			() => { setTimeout(function(){ localStorage.setItem("_note", app.note.value); }, 500) }
-		);
+		  );
 		
   		if(localStorage.getItem("_note") && localStorage.getItem("_note")!=''){
     		var noteItem = localStorage.getItem("_note");
     		app.note.value = noteItem;
   		}
+
+      screen.addEventListener("orientationchange", function () {
+        alert("The orientation of the screen is: " + screen.orientation);
+      });
 
 	  	app.clearNotes.addEventListener('click', (event) => {
         event.preventDefault();
